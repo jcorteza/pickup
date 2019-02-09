2d30505563f1a6b4135fc7c41e800d42b76bc062
JCortezAyala <40844882+jcorteza@users.noreply.github.com>
2019-02-09 00:31:44 -0600

    removed proxy

--- client/src/setupProxy.js
+++
@@ -1,6 +0,0 @@
-const proxy = require('http-proxy-middleware');
-// add to package.json  "proxy": "http://localhost:3001/",
-
-module.exports = function(app) {
-    app.use(proxy('/api', { target: 'http://localhost:3001/' }));
-};
